d3cf5ba9b13106ab79d40c3b9a66e4a9a05ce324
deaththekidd <ohiemidivine7@gmail.com>
2023-09-21 00:08:59 +0100

    [update]

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,19 @@
+const User = require("../models/user.model");
+
 exports.allAccess = (req, res) => {
 	res.status(200).send("Public Content.");
 };
 
-exports.userBoard = (req, res) => {
-	res.status(200).send("User Content.");
+exports.userBoard = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) return res.status(404).json({ error: "User not found" });
+		return res
+			.status(200)
+			.json({ message: "User fetched succesfully", data: user });
+	} catch (error) {
+		return res.status(500).json({ error });
+	}
 };
 
 exports.adminBoard = (req, res) => {
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
 
 	app.get("/api/test/all", controller.allAccess);
 
-	app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+	app.get("/api/test/user/:id", controller.userBoard);
 
 	app.get(
 		"/api/test/mod",
