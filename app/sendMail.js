88255568fc8774b7ef5880f0355a6af2471787ad
Divine Attah-Ohiemi <ohiemidivine7@gmail.com>
2023-12-04 17:24:35 +0100

    update

--- app/sendMail.js
+++ app/sendMail.js
@@ -4,47 +4,38 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const createTransporter = async () => {
-	const transporter = nodemailer.createTransport({
-		host: "smtp-relay.brevo.com",
-		port: 587,
-		auth: {
-			user: process.env.SENDER_EMAIL,
-			pass: process.env.SENDER_PASS,
-		},
-	});
+const Sib = require("sib-api-v3-sdk");
 
-	return transporter;
-};
+require("dotenv").config();
 
-async function sendMail(to, subject, text) {
+const client = Sib.ApiClient.instance;
+
+const apiKey = client.authentications["api-key"];
+apiKey.apiKey = process.env.SIB_API_KEY;
+console.log(process.env.SIB_API_KEY);
+const tranEmailApi = new Sib.TransactionalEmailsApi();
+
+async function sendMail(receivers, subject, text) {
+	const sender = {
+		email: process.env.SENDER_EMAIL,
+		name: "Coin Venture Support",
+	};
 	let mailOptions = {
-		from: process.env.SENDER_EMAIL, // sender address
-		to: to, // list of receivers
+		sender, // sender address
+		to: [{ email: receivers }], // list of receivers
 		subject: subject, // Subject line
-		text: text, // plain text body
+		textContent: text, // plain text body
 	};
 
-	console.log(mailOptions);
-
 	try {
-		// Get response from the createTransport
-		let emailTransporter = await createTransporter();
-
-		// Send email
-		await new Promise((resolve, reject) => {
-			emailTransporter.sendMail(mailOptions, function (error, info) {
-				if (error) {
-					// failed block
-					console.log(error);
-					reject(error);
-				} else {
-					// Success block
-					console.log("Email sent: " + info.response);
-					resolve(info);
-				}
+		tranEmailApi
+			.sendTransacEmail(mailOptions)
+			.then(() => {
+				console.log("Email sent:");
+			})
+			.catch((error) => {
+				console.log(error);
 			});
-		});
 	} catch (error) {
 		return console.log(error);
 	}
